2ca426750e7de46aff77767c2b6976210d2a70b0
xiepanpan <xiepanpan@thunisoft.com>
2020-05-27 18:59:43 +0800

    Initial commit

---
+++ src/main/java/com/imooc/netty/websocket/ChatHandler.java
@@ -0,0 +1,18 @@
+package com.imooc.netty.websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+/**
+ * @author: xiepanpan
+ * @Date: 2020/5/27
+ * @Description:  处理消息的handler  TextWebSocketFrame： 在netty中，是用于为websocket专门处理文本的对象，frame是消息的载体
+ */
+public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        String content = msg.text();
+        System.out.println("接收到的数据："+content);
+    }
+}
